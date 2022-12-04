53bdf0d328e1c460b0ecf7f5535e2e91eb26787f
Meatball <meatballsofmeatballs@gmail.com>
2022-12-04 10:35:46 +0100

    something

---
+++ exercises/practice/state-of-tic-tac-toe/state_of_tic_tac_toe_test.py
@@ -0,0 +1,64 @@
+import unittest
+
+from state_of_tic_tac_toe import (
+    gamestate,
+)
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+
+class StateOfTicTacToeTest(unittest.TestCase):
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
+
+    {test_case(casey)}
